ac259cd8c7cdd6188a0bdbe2b9ad56c02cdc80c3
Brian La <133820463+bla29@users.noreply.github.com>
2026-02-04 16:17:12 -0800

    add/delete logic and list structure in js done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import add from "./add.js"
 import remove from "./remove.js"
 
 class Project {
+    todoItems = [];
     constructor(projectTitle) {
         this.projectTitle = projectTitle;
     }
 }
+
+Object.assign(Project.prototype, add);
+Object.assign(Project.prototype, remove);
 
 class Todo extends Project {
     constructor(title, description, dueDate, priority, projectTitle) {
@@ -18,4 +22,11 @@ class Todo extends Project {
 }
 
 let cleanCar = new Todo('Clean Carpet', 'Vaccum the carpets', '02/12/26', 'Medium', 'Clean Car');
-console.log(cleanCar);
+let changeOil = new Todo('Change Oil', 'Change car engine oil', '05/12/26', 'High', 'Clean Car');
+
+let carMaintenance = new Project('Clean Car');
+carMaintenance.add(carMaintenance.todoItems, cleanCar)
+carMaintenance.add(carMaintenance.todoItems, changeOil)
+
+carMaintenance.remove(carMaintenance.todoItems, cleanCar)
+console.log(carMaintenance)
--- src/remove.js
+++ src/remove.js
@@ -1,6 +1,10 @@
 export default {
-    remove(list, itemIndex) {
-        list.splice(itemIndex, 1);
+    remove(list, item) {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].title == item.title) {
+                list.splice(i, 1);
+            }
+        }
         return list;
     }
 }
